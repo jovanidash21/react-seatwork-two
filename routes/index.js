70f13fda79f8c2749b507f2c03edd5813456518e
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-08-14 21:28:08 +0800

    Update Page Title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Seatwork Two' });
+  res.render('index', { title: 'React - Seatwork Two' });
 });
 
 module.exports = router;
